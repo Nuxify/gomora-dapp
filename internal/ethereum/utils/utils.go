c2f23b0c3691fe1be7f0e6acb3c8df44aa7d17e6
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2023-11-23 15:17:47 +0800

    feat(internal): add ethereum utils

---
+++ internal/ethereum/utils/utils.go
@@ -0,0 +1,41 @@
+// reference: https://github.com/ethereum/go-ethereum/issues/21221#issuecomment-802092592
+package utils
+
+import (
+	"fmt"
+	"math/big"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/params"
+)
+
+// EtherToWei converts ether to bigint
+func EtherToWei(eth *big.Float) *big.Int {
+	truncInt, _ := eth.Int(nil)
+	truncInt = new(big.Int).Mul(truncInt, big.NewInt(params.Ether))
+	fracStr := strings.Split(fmt.Sprintf("%.18f", eth), ".")[1]
+	fracStr += strings.Repeat("0", 18-len(fracStr))
+	fracInt, _ := new(big.Int).SetString(fracStr, 10)
+	wei := new(big.Int).Add(truncInt, fracInt)
+	return wei
+}
+
+// ParseBigFloat parse string value to big.Float
+func ParseBigFloat(value string) (*big.Float, error) {
+	f := new(big.Float)
+	f.SetPrec(236) //  IEEE 754 octuple-precision binary floating-point format: binary256
+	f.SetMode(big.ToNearestEven)
+	_, err := fmt.Sscan(value, f)
+	return f, err
+}
+
+// WeiToEther converts wei to ether
+func WeiToEther(wei *big.Int) *big.Float {
+	f := new(big.Float)
+	f.SetPrec(236) //  IEEE 754 octuple-precision binary floating-point format: binary256
+	f.SetMode(big.ToNearestEven)
+	fWei := new(big.Float)
+	fWei.SetPrec(236) //  IEEE 754 octuple-precision binary floating-point format: binary256
+	fWei.SetMode(big.ToNearestEven)
+	return f.Quo(fWei.SetInt(wei), big.NewFloat(params.Ether))
+}
