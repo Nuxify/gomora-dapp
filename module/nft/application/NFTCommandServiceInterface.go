535dae6737bff2f7f97834c01b62d5109230f407
Andrea Ella Remoreras <ae.remoreras@gmail.com>
2025-02-07 14:09:12 +0800

    docs: add comments in nft interface methods

--- module/nft/application/NFTCommandServiceInterface.go
+++ module/nft/application/NFTCommandServiceInterface.go
@@ -8,5 +8,6 @@ import (
 
 // NFTCommandServiceInterface holds the implementable methods for the nft command service
 type NFTCommandServiceInterface interface {
+	// CreateNFTLogSetGreeting creates a new log set greeting event
 	CreateNFTLogSetGreeting(ctx context.Context, txHash string, logIndex uint, contractAddress string, data types.CreateNFTLogSetGreeting) error
 }
--- module/nft/domain/repository/NFTCommandRepositoryInterface.go
+++ module/nft/domain/repository/NFTCommandRepositoryInterface.go
@@ -4,5 +4,6 @@ import repositoryTypes "gomora-dapp/module/nft/infrastructure/repository/types"
 
 // NFTCommandRepositoryInterface holds the implementable methods for the nft command repository
 type NFTCommandRepositoryInterface interface {
+	// InsertGreeterContractEventLog inserts a greeter contract event log
 	InsertGreeterContractEventLog(data repositoryTypes.CreateGreeterContractEventLog) error
 }
