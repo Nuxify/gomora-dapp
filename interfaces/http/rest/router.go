f48c168e2e298e15d008e94af2ee0f52310d22ec
Andrea Ella Remoreras <ae.remoreras@gmail.com>
2025-05-21 16:37:44 +0800

    feat: harcode api version in router, also update in openapi docs

--- interfaces/http/rest/router.go
+++ interfaces/http/rest/router.go
@@ -61,7 +61,7 @@ func (router *router) InitRouter() *chi.Mux {
 			Status:  http.StatusOK,
 			Success: true,
 			Message: "alive",
-			Data:    map[string]string{"version": os.Getenv("API_VERSION")},
+			Data:    map[string]string{"version": "v1.7.3"},
 		}
 
 		response.JSON(w)
