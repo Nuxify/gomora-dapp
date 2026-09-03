bbdeefc1c7f51060ee47adaa6e178ef069aa1d3a
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2026-09-03 08:30:51 +0800

    chore: proper naming and version bump

--- interfaces/http/rest/router.go
+++ interfaces/http/rest/router.go
@@ -61,7 +61,7 @@ func (router *router) InitRouter() *chi.Mux {
 			Status:  http.StatusOK,
 			Success: true,
 			Message: "alive",
-			Data:    map[string]string{"version": "v1.9.2"},
+			Data:    map[string]string{"version": "v1.10.0"},
 		}
 
 		response.JSON(w)
